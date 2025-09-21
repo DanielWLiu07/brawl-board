7828d9b49c4fb225074d2da09c896ba6ffdace67
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-09-20 23:20:26 -0400

    refactor: render WhiteBoardCreationPage instead of default App component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import WhiteBoardCreationPage from './pages/WhiteBoardCreationPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WhiteBoardCreationPage />
   </StrictMode>,
 )
