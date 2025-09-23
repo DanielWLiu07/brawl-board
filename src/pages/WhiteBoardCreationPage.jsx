d5d73b2c055142c49492bca7f05cbb2354240cd2
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-09-22 23:17:42 -0400

    feat: add CanvasBoard component

--- src/pages/WhiteBoardCreationPage.jsx
+++ src/pages/WhiteBoardCreationPage.jsx
@@ -1,4 +1,6 @@
 import "./WhiteBoardCreationPage.css";
+import CanvasBoard from "../components/CanvasBoard"; 
+
 
 const WhiteBoardCreationPage = () => {
 	return (
@@ -10,13 +12,14 @@ const WhiteBoardCreationPage = () => {
 			</div>
 			<div className="middle-section">
 				<div className="whiteboard-aspect">
-					
+					<CanvasBoard />
 				</div>
 			</div>
 			<div className="right-section">
 				<div className="brawler-icons">
 					<h2>Brawler Icons</h2>
-					{/* Add brawler icons here */}
+
+
 				</div>
 				<div className="attack-cones">
 					<h2>Attack Cones</h2>
