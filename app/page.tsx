81ef570b3d92140cfb8e478785524c38361e7047
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-06 22:48:57 -0400

    feat: add basic title text

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <Button size = "sm" variant = "destructive">
+      Brawl Stars White Board
       Click me
+
     </Button>
   );
 }
