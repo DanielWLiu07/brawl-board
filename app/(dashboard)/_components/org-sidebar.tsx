e900429c1095a1f6454422d0e810750b9a06ccad
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-17 23:37:23 -0400

    feat: add Poppins font to orgnization sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google"; 
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+})
+
 export const OrgSidebar = () => {
     return(
         <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
             Org Sidebar
-
+            <Link href = "/">
+            </Link>
         </div>
     )
 
