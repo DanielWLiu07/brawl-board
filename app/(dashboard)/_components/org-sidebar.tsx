f6f3f8026d04a1c89071c596b02f67ba4be8e2d8
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-17 23:47:10 -0400

    feat: add logo and name to org sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Poppins } from "next/font/google"; 
+import { cn } from "@/lib/utils";
 
 const font = Poppins({
     subsets: ["latin"],
@@ -12,8 +13,19 @@ const font = Poppins({
 export const OrgSidebar = () => {
     return(
         <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-            Org Sidebar
             <Link href = "/">
+                <div className="flex items-center gap-x-2">
+                    <Image 
+                        src = "/temp-logo.png"
+                        alt = "Logo"
+                        width={60}
+                        height={60}
+                    />
+                    <span className={cn("font-semibold text-2xl", font.className)}>
+                        Board
+
+                    </span>
+                </div>
             </Link>
         </div>
     )
