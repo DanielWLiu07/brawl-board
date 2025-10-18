f3195888fbc401d4fb713a3cf77e3889233696aa
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-17 23:49:25 -0400

    feat: add ability to swap organizations in org sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Poppins } from "next/font/google"; 
 import { cn } from "@/lib/utils";
-
+import { OrganizationSwitcher } from "@clerk/nextjs";
 const font = Poppins({
     subsets: ["latin"],
     weight: ["600"],
@@ -27,6 +27,7 @@ export const OrgSidebar = () => {
                     </span>
                 </div>
             </Link>
+            <OrganizationSwitcher />
         </div>
     )
 
