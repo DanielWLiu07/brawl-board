886ee13ebeff5b19728c91261d32a6d530d25aac
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-19 23:30:57 -0400

    feat: change button visual on click

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -7,12 +7,17 @@ import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, Star} from "lucide-react";
+import { useSearchParams} from "next/navigation"
+
 const font = Poppins({
     subsets: ["latin"],
     weight: ["600"],
 })
 
 export const OrgSidebar = () => {
+    const searchParams = useSearchParams();
+    const favorites = searchParams.get("favorites");
+    
     return(
         <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-white">
             <Link href = "/">
@@ -51,7 +56,7 @@ export const OrgSidebar = () => {
             }}/>
             <div className="space-y-1 w-full">
                 <Button
-                variant = "ghost"
+                variant = {favorites ? "ghost" : "secondary"}
                 asChild
                 size="lg"
                 className="font=normal justify-start px-2 w-full">
@@ -62,13 +67,14 @@ export const OrgSidebar = () => {
                     </Link>
                 </Button>
                 <Button
-                variant = "ghost"
+                variant = {favorites ? "secondary" : "ghost"}
                 asChild
                 size="lg"
                 className="font=normal justify-start px-2 w-full">
-                    <Link href="
-                    pathname:"" 
-                    ">
+                    <Link href={{
+                        pathname:'/',
+                        query: {favorites: true}
+                    }}>
                     <Star className="h-4 w-4 mr-2"/>
                     Fav boards
                         
