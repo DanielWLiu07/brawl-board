4ad1d6f0a01a6bdb348615042bc7d74c8016cee2
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-21 23:47:20 -0400

    chore: manage imports for search_input

--- app/(dashboard)/_components/search_input.tsx
+++ app/(dashboard)/_components/search_input.tsx
@@ -1,4 +1,15 @@
-"use client"
+"use client";
+
+import qs from "query-string";
+import { Search } from "lucide-react";
+import { useDebounce } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+
+import {
+    ChangeEvent,
+    useEffect,
+    useState
+} from "react";
 
 export const SearchInput = () => {
     return (
