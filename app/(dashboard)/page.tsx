d4d676863cf2f4d2c872f5dc6b52c4e3cac7f7d0
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-27 23:20:43 -0400

    feat: conditional empty org display

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 const DashboardPage = () => {
+    const { organization } = useOrganization();
+
     return (
         <div className="flex-1 h-[calc(100%-80px)] p-6">
-            <EmptyOrg />
+            {!organization ? (
+                <EmptyOrg />
+            ) : (
+                <div>
+                    board_list
+                </div>
+            )}
         </div>
     );
 };
