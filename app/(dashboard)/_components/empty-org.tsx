0b79cb44e5f31de2bc552db002065e2cd5b5aee4
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-10-27 23:18:12 -0400

    feat: create empty org screen for when no org joined

--- app/(dashboard)/_components/empty-org.tsx
+++ app/(dashboard)/_components/empty-org.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
+import {
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    DialogTrigger,
+
+} from "@/components/ui/dialog"
+
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+import { CreateOrganization } from "@clerk/nextjs";
 export const EmptyOrg = () => {
     return(
         <div className = "h-full flex flex-col items-center justify-center">
-            Empty
             <Image
                 src = "/globe.svg"
                 alt="Missing Org"
@@ -16,6 +27,22 @@ export const EmptyOrg = () => {
             <p className= "text-muted-foreground mt-2 text-sm">
                 Create an Organization to get started!
             </p>
+            <div className="mt-6">
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size="lg">
+                            Create Orgnization
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="p-0 bg-transparent border-none w-auto">
+                        <VisuallyHidden>
+                            <DialogTitle>Create Organization</DialogTitle>
+                        </VisuallyHidden>
+                        <CreateOrganization />
+                    </DialogContent>
+                </Dialog>
+
+            </div>
             
         
         </div>
