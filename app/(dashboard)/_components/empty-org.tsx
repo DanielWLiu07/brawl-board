47ce348286075441ddc477f4f587dc49866a39a4
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-12-15 20:55:02 -0500

    fix: correct typo and clean up empty-org component

--- app/(dashboard)/_components/empty-org.tsx
+++ app/(dashboard)/_components/empty-org.tsx
@@ -1,22 +1,19 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
 import {
     Dialog,
     DialogContent,
     DialogTitle,
     DialogTrigger,
-
-} from "@/components/ui/dialog"
-
+} from "@/components/ui/dialog";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-
 import { CreateOrganization } from "@clerk/nextjs";
+
 export const EmptyOrg = () => {
-    return(
-        <div className = "h-full flex flex-col items-center justify-center">
+    return (
+        <div className="h-full flex flex-col items-center justify-center">
             <Image
-                src = "/globe.svg"
+                src="/globe.svg"
                 alt="Missing Org"
                 height={200}
                 width={200}
@@ -24,14 +21,14 @@ export const EmptyOrg = () => {
             <h2 className="text-2xl font-semibold mt-6">
                 Welcome to Board
             </h2>
-            <p className= "text-muted-foreground mt-2 text-sm">
+            <p className="text-muted-foreground mt-2 text-sm">
                 Create an Organization to get started!
             </p>
             <div className="mt-6">
                 <Dialog>
                     <DialogTrigger asChild>
                         <Button size="lg">
-                            Create Orgnization
+                            Create Organization
                         </Button>
                     </DialogTrigger>
                     <DialogContent className="p-0 bg-transparent border-none w-auto">
@@ -41,12 +38,7 @@ export const EmptyOrg = () => {
                         <CreateOrganization />
                     </DialogContent>
                 </Dialog>
-
             </div>
-            
-        
         </div>
-    )
-
-
-}
+    );
+};
